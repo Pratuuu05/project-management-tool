840ea9576de505b92337c1ce4c293d991d5fc1ad
Pratuuu05 <avcharpratiksha@gmail.com>
2026-06-07 15:15:26 +0530

    Allow frontend CORS

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://project-management-tool-flax.vercel.app'
-  ],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
